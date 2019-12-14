c42734e72f990e3fbb09a1993c1d076a7049c7a5
Francis Ilechukwu <francis94c@gmail.com>
2019-12-14 15:04:30 +0100

    AsyncJob class.

---
+++ libraries/AsyncJob.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+abstract class AsyncJob
+{
+  /**
+   * [protected description]
+   * @var [type]
+   */
+  protected $args;
+  /**
+   * [__construct description]
+   * @date  2019-12-03
+   * @param array      $args [description]
+   */
+  function __construct(...$args)
+  {
+    $this->args = $args;
+  }
+  /**
+   * [handle description]
+   * @date 2019-12-04
+   */
+  abstract public function execute():void;
+}
